17c8a40661580747ee15809ba79e2e5001bbd30f
Michal Kujawski <michalkujawski@workfront.com>
2018-11-24 15:25:28 +0100

    added base for card types

---
+++ data.py
@@ -0,0 +1,32 @@
+from discord import Embed
+
+
+class Card:
+    pass
+
+    def to_embed(self) -> Embed:
+        pass
+
+
+class CardSpell(Card):
+    pass
+
+
+class CardHero(Card):
+    pass
+
+
+class CardCreep(Card):
+    pass
+
+
+class CardAbility(Card):
+    pass
+
+
+class CardPassiveAbility(Card):
+    pass
+
+
+class CardItem(Card):
+    pass
