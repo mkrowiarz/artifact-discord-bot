0ef9bdcc2c75f2b0168a1a2d0f225a05e21f93e2
Michal Kujawski <michalkujawski@workfront.com>
2018-11-24 14:39:52 +0100

    removed asyncio

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 # Work with Python 3.6
 import discord
-import asyncio
 
 TOKEN = 'NTE1ODMxMjUwNDM1NTcxNzIz.DtrZHw.vVr3gOnAa5sl8YnG0tdDuks9T5Y'
 
@@ -8,20 +7,18 @@ client = discord.Client()
 
 
 @client.event
-@asyncio.coroutine
-def on_message(message):
+async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
     if message.content.startswith('!c'):
         msg = f'Hello {message.author.mention}'
-        yield from client.send_message(message.channel, msg)
+        await client.send_message(message.channel, msg)
 
 
 @client.event
-@asyncio.coroutine
-def on_ready():
+async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
