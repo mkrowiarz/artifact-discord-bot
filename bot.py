aba1334f47192b39773c26d97cd71d9d21e81554
Michal Kujawski <michalkujawski@workfront.com>
2018-11-24 15:01:50 +0100

    switched to @client.command with discord.ext.command.Bot functionality instead of analysing all messages "by hand"

--- bot.py
+++ bot.py
@@ -1,20 +1,26 @@
 # Work with Python 3.6
 import discord
+from discord.ext.commands import Bot
 
 TOKEN = 'NTE1ODMxMjUwNDM1NTcxNzIz.DtrZHw.vVr3gOnAa5sl8YnG0tdDuks9T5Y'
 
-client = discord.Client()
+client = Bot(command_prefix='!')
 
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+@client.command()
+async def card(card_name):
+    # Du medżik
+    await client.say("Chaka Laka Pocałuj Mnie W Ptaka")
 
-    if message.content.startswith('!c'):
-        msg = f'Hello {message.author.mention}'
-        await client.send_message(message.channel, msg)
+# @client.event
+# async def on_message(message):
+#     # we do not want the bot to reply to itself
+#     if message.author == client.user:
+#         return
+#
+#     if message.content.startswith('!c'):
+#         msg = f'Hello {message.author.mention}'
+#         await client.send_message(message.channel, msg)
 
 
 @client.event
